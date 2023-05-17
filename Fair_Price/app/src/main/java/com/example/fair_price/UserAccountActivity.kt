63070a917eaa0800b69383b56e5000de1b869e9d
Ashen Madhusanka <99417273+ashen0909@users.noreply.github.com>
2023-05-17 08:23:03 +0530

    addPaye

---
+++ Fair_Price/app/src/main/java/com/example/fair_price/UserAccountActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fair_price
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_account)
+    }s
+}
